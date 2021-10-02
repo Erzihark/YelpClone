6a1e07831c95caa7521de1bfa25cb1c5e07d1554
ManuelCarretero <manuel.carretero@undostres.com.mx>
2021-10-01 20:19:06 -0500

    CSS general styling

--- my-app/client/src/components/Intro.js
+++ my-app/client/src/components/Intro.js
@@ -1,15 +1,13 @@
 import React from "react";
-import banner from "../images/banner.jpeg"
 
 export default function Intro(){
     return (
-        <header className={"intro"}>
-            <div className={"intro-img-container"}>
-                <p className={"intro-text"}>
+        <section className={"intro"}>
+            <div className={"intro-text__container"}>
+                <h2 className={"intro-text"}>
                     Pls just buy our nft we live in a 3rd world wasteland and need money to barely survive
-                </p>
-                <img src={banner} alt={"header image"}/>
+                </h2>
             </div>
-        </header>
+        </section>
     )
 }
